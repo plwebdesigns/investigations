97ef089564b3324574fce4f4c150ab5b8abf16a9
plongo <paullongo@outlook.com>
2018-02-11 12:18:05 -0500

    added screenshot for theme

---
+++ wp-content/themes/investigations/404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template file for 404, page not found
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Custom theme
+ * @subpackage pro_investigations
+ * @since 1.0
+ * @version 1.0
+ */
+get_header();
+?>
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
