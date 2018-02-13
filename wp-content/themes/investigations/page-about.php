795b13e8e417a1d485fb9f5d852b0bcfa01d69b6
plongo <paullongo@outlook.com>
2018-02-13 16:59:17 -0500

    added bio section to about page

--- wp-content/themes/investigations/page-about.php
+++ wp-content/themes/investigations/page-about.php
@@ -13,19 +13,35 @@
 get_header();
 ?>
 
-	<div class="container-fluid">
-		<div class="page-content">
-			<?php if (have_posts()) :
-				while (have_posts()) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
-					<?php the_content(); ?>
-				<?php endwhile; ?>
-			<?php endif; ?>
-		</div>
+<!--Get ACFs for About Page-->
+<?php
+$profile = get_field('profile_pic');
+$bio_header = get_field('bio_header');
+$bio = get_field('bio');
+?>
+
+	<div class="container">
+        <div class="row about-content">
+            <div class="col-8">
+                <?php if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_content(); ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+            <div class="about-bio col-4">
+                <figure>
+	                <?php echo wp_get_attachment_image($profile, 'medium'); ?>
+                </figure>
+                <h4><? echo $bio_header; ?></h4>
+                <p><? echo $bio; ?></p>
+            </div>
+        </div>
 	</div>
     <div class="container">
         <div class="row justify-content-center about-footer">
-            <div class="col-4">
+            <div class="col-6">
                 <h4>Interested in working with us? </h4>
             </div>
             <div class="col-4">
